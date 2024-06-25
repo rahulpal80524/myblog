deaf144efd7efed643fb30bf7510f750e719355d
RP <rahulpal80524@gmail.com>
2024-06-25 21:07:27 +0530

    typo mistake removed

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,9 @@ const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollectionId: String("6679078600308d31cc87"),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
    
 }
-
 
 export default conf
